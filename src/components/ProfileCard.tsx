69790709ecf2a8152f0592b5daa88fd1cae851cb
Renan Borges <51171566+borgesrenan@users.noreply.github.com>
2023-05-12 21:16:30 +0100

    Adding some images and creating gestures

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,67 @@
+import { IonCard, IonImg, IonText, createGesture } from '@ionic/react';
+import React, { useEffect } from 'react';
+import './ProfileCard.css'
+
+
+interface ContainerProps {
+    image: string
+    name: string
+    bio: string
+    onMatch: () => void
+    onUnmatch: () => void
+    onReset: () => void
+}
+
+const ProfileCard: React.FC<ContainerProps> = (props) => {
+
+    const ref = React.useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        gestureInit()
+    }, [])
+
+    const gestureInit = () => {
+        const card = ref.current;
+        if (card) {
+            const gesture = createGesture({
+                el: card,
+                gestureName: 'card-swipe',
+                onMove: (detail) => {
+                    card.style.transform = `translateX(${detail.deltaX}px) rotate(${detail.deltaX / 20
+                        }deg)`
+                },
+                onEnd: (detail) => {
+                    const windowWidth = window.innerWidth
+                    card.style.transition =
+                        '0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275)'
+                    if (detail.deltaX > windowWidth / 2) {
+                        card.style.transform = `translateX(${windowWidth * 1.5}px)`
+                    } else if (detail.deltaX < -windowWidth / 2) {
+                        card.style.transform = `translateX(-${windowWidth * 1.5}px)`
+                    } else {
+                        card.style.transform = ''
+                    }
+                },
+            })
+            gesture.enable()
+        }
+    }
+
+    return (
+        <div ref={ref}>
+            <IonCard mode="ios">
+                <div className='card-container'>
+                    <IonImg src={props.image} />
+                </div>
+                <div className='details-container'>
+                    <IonText className='name'>{props.name}</IonText>
+                    <IonText className='bio'>
+                        <p>{props.bio}</p>
+                    </IonText>
+                </div>
+            </IonCard>
+        </div>
+    );
+};
+
+export default ProfileCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,40 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
 import ActionButtons from '../components/ActionButtons';
+import ProfileCard from '../components/ProfileCard';
 
 const Home: React.FC = () => {
+  const profileEvents = {
+    onMatch: () => { },
+    onUnmatch: () => { },
+    onReset: () => { }
+  }
+
+  const profiles = [
+    {
+      id: 1,
+      image: "https://tudosobrecachorros.com.br/wp-content/uploads/cachorros-fofos-04-1024x640.jpg",
+      name: "Dog1",
+      bio: "loren ipsumloren ipsumloren ipsumloren ipsum",
+      ...profileEvents
+    },
+    {
+      id: 2,
+      image: "https://www.petz.com.br/blog/wp-content/uploads/2020/11/pode-dar-plasil-para-cachorro.jpg",
+      name: "Dog2",
+      bio: "loren ipsumloren ipsumloren ipsumloren ipsum",
+      ...profileEvents
+    },
+    {
+      id: 3,
+      image: "https://tudosobrecachorros.com.br/wp-content/uploads/cachorros-fofos-11-1024x768.jpg",
+      name: "Dog3",
+      bio: "loren ipsumloren ipsumloren ipsumloren ipsum",
+      ...profileEvents
+    }
+
+  ]
+
   return (
     <IonPage>
       <IonHeader mode="ios">
@@ -11,8 +43,13 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-       
-       <ActionButtons/>
+        <div className='cards-stack-container'>
+          {profiles.map(profile => (
+            <ProfileCard {...profile} key={profile.id} />
+          ))}
+        </div>
+
+        <ActionButtons />
       </IonContent>
     </IonPage>
   );
